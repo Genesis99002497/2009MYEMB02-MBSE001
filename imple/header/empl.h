c22a05c4f2ddc0e4f784adeaa15662fb13715053
Genesis99002497 <71490682+Genesis99002497@users.noreply.github.com>
2020-09-19 14:30:35 +0530

    Update empl.h

--- imple/header/empl.h
+++ imple/header/empl.h
@@ -2,15 +2,11 @@
 #define __EMPL_H__
 
 
-//#include<stdio.h>
-//#include<string.h>
-//#include<stdlib.h>
-//#include<math.h>
-#include <stdio.h> 
-#include <stdlib.h>
-#include <conio.h>
-#include <windows.h> 
-#include <string.h>  
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<math.h>
+
 
 
 #endif
